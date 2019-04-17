d92344876f9496f42c1b8fd516a788fbcb8e5f33
Alan Barahona Ruiz <barahonaruiz.alan@gmail.com>
2019-04-17 08:47:46 -0400

    Lab1 formateado

--- lab1/src/main.py
+++ lab1/src/main.py
@@ -1,32 +1,32 @@
 # -*- coding: utf-8 -*-
 import matplotlib.pyplot as plt
 import numpy as np
-from numpy import pi
 from scipy.fftpack import fft, fftfreq, ifft
 from scipy.io.wavfile import read, write
 
-# File
-file = read("../resources/handel.wav")
+# Obtencion de informacion
+file = read("../resources/audio/handel.wav")
 rate = file[0]
 amplitudeTime = file[1]
 dataLen = len(amplitudeTime)
-duration = dataLen/rate
 delta = 1/rate
 time = np.linspace(0, (dataLen - 1) * delta, dataLen)
 amplitudeFrequency = fft(amplitudeTime)
 frequency = fftfreq(dataLen, delta)
-inverseAmplitude = ifft(amplitudeFrequency).real
-fakedAmplitude = amplitudeFrequency.copy()
+inverseAmplitudeTime = ifft(amplitudeFrequency).real
+fakedAmplitudeFrequency = amplitudeFrequency.copy()
 for x in range(12504,36557):
-    fakedAmplitude[x] = 0;
-    fakedAmplitude[73113 - x] = 0
+    fakedAmplitudeFrequency[x] = 0;
+    fakedAmplitudeFrequency[73113 - x] = 0
 for x in range(2000):
-    fakedAmplitude[x] = 0;
-    fakedAmplitude[73112 - x] = 0
-inverseFakedAmplitude = ifft(fakedAmplitude).real
-firstQuadraticError = np.sqrt(((inverseAmplitude - amplitudeTime) ** 2).mean())
-secondQuadraticError = np.sqrt(((inverseFakedAmplitude - amplitudeTime) ** 2).mean())
-write("../resources/handelTruncado.wav",rate,inverseFakedAmplitude)
+    fakedAmplitudeFrequency[x] = 0;
+    fakedAmplitudeFrequency[73112 - x] = 0
+inverseFakedAmplitudeTime = ifft(fakedAmplitudeFrequency).real
+write("../resources/audio/handelTruncado.wav",rate,inverseFakedAmplitudeTime)
+
+# Cálculo de errores
+firstQuadraticError = np.sqrt(((inverseAmplitudeTime - amplitudeTime) ** 2).mean())
+secondQuadraticError = np.sqrt(((inverseFakedAmplitudeTime - amplitudeTime) ** 2).mean())
 
 # # Gráfico de la transformada de fourier
 plt.figure(1)
@@ -35,12 +35,12 @@ plt.title("Señal original")
 plt.xlabel("Tiempo (s)")
 plt.ylabel("Amplitud")
 plt.figure(2)
-plt.plot(time,inverseAmplitude)
+plt.plot(time,inverseAmplitudeTime)
 plt.title("Señal invertida")
 plt.xlabel("Tiempo (s)")
 plt.ylabel("Amplitud")
 plt.figure(3)
-plt.plot(time,inverseFakedAmplitude)
+plt.plot(time,inverseFakedAmplitudeTime)
 plt.title("Señal invertida truncada")
 plt.xlabel("Tiempo (s)")
 plt.ylabel("Amplitud")
@@ -50,9 +50,8 @@ plt.title("Transformada de Fourier")
 plt.xlabel('Frecuencia (Hz)')
 plt.ylabel('Amplitud')
 plt.figure(5)
-plt.vlines(frequency, 0, np.abs(fakedAmplitude))
+plt.vlines(frequency, 0, np.abs(fakedAmplitudeFrequency))
 plt.title("Transformada de Fourier truncada")
 plt.xlabel('Frecuencia (Hz)')
 plt.ylabel('Amplitud')
-
 plt.show()
