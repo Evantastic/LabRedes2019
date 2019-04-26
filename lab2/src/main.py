4f5a247548427fa1275d5ad9d778ebcbd8bfec1f
Alan Barahona Ruiz <alan.barahona@usach.cl>
2019-04-25 21:47:25 -0400

    test

--- lab2/src/main.py
+++ lab2/src/main.py
@@ -7,6 +7,8 @@ import numpy as np
 rate, amplitudeTime = read("../resources/audio/handel.wav")
 frequencyTime = fft(amplitudeTime)
 f,t,spectrum = spectrogram(amplitudeTime, rate)
+
+
 plt.pcolormesh(t,f,np.log10(spectrum))
 plt.title("Espectrograma")
 plt.xlabel("Tiempo [s]")
