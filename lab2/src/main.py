4a90b75aaf4ff1a136e9dee70b0f6ca90c8a574a
Alan Barahona Ruiz <alan.barahona@usach.cl>
2019-04-27 17:48:08 -0400

    Calculo de filtros

--- lab2/src/main.py
+++ lab2/src/main.py
@@ -36,20 +36,29 @@ inverseAmplitudeTime = [None] * 6
 fs = [None] * 6
 ts = [None] * 6
 spectrums = [None] * 6
-for i in range(3):
-    j = 3 + i
-    w[i], h[i] = pretty_bandpass(560, 1125, rate, 3 * i + 1, maximum=2.400e7)
-    w[j], h[j] = pretty_bandpass(280, 1670, rate, 3 * i + 1, maximum=2.400e7)
-    filteredFrequencies[i] = filter(amplitudeFrequency, 560, 1125, rate, i + 1)
-    filteredFrequencies[j] = filter(amplitudeFrequency, 280, 1670, rate, i + 1)
+for i in range(2):
+    j = 2 + i
+    k = 4 + i
+    w[i], h[i] = pretty_bandpass(280, 4000, rate, 5 * (i + 1), maximum=2.400e7)
+    w[j], h[j] = pretty_bandpass(280, 1670, rate, 5 * (i + 1), maximum=2.400e7)
+    w[k], h[k] = pretty_bandpass(1, 1670, rate, 5 * (i + 1), maximum=2.400e7)
+    filteredFrequencies[i] = filter(amplitudeFrequency, 280, 4000, rate, 5*(i + 1))
+    filteredFrequencies[j] = filter(amplitudeFrequency, 280, 1670, rate, 5*(i + 1))
+    filteredFrequencies[k] = filter(amplitudeFrequency, 1, 1670, rate, 5*(i + 1))
     inverseAmplitudeTime[i] = np.asarray(ifft(filteredFrequencies[i]).real, dtype=np.int16)
     inverseAmplitudeTime[j] = np.asarray(ifft(filteredFrequencies[j]).real, dtype=np.int16)
+    inverseAmplitudeTime[k] = np.asarray(ifft(filteredFrequencies[k]).real, dtype=np.int16)
     fs[i], ts[i], spectrums[i] = spectrogram(inverseAmplitudeTime[i], rate)
     fs[j], ts[j], spectrums[j] = spectrogram(inverseAmplitudeTime[j], rate)
+    fs[k], ts[k], spectrums[k] = spectrogram(inverseAmplitudeTime[k], rate)
 
 # Guardar archivos
-for i in range(6):
-    write("../resources/audio/handelFiltrado%d.wav"%(i + 1), rate, inverseAmplitudeTime[i])
+for i in range(2):
+    j = 2 + i
+    k = 4 + i
+    write("../resources/audio/handelHighOrden%d.wav"%(5*(i + 1)), rate, inverseAmplitudeTime[i])
+    write("../resources/audio/handelLowOrden%d.wav"%(5*(i + 1)), rate, inverseAmplitudeTime[k])
+    write("../resources/audio/handelBandOrden%d.wav"%(5*(i + 1)), rate, inverseAmplitudeTime[j])
 
 # Primer espectrograma
 plt.figure(1)
@@ -63,10 +72,12 @@ plt.colorbar().set_label("Amplitud [Db]")
 plt.figure(3)
 plt.clf()
 plt.plot(frequency, np.abs(amplitudeFrequency), label="original")
-for i in range(3):
-    j = 3 + i
-    plt.plot(w[i], h[i], label="orden = %d"%i)
-    plt.plot(w[j], h[j], label="orden = %d"%j)
+for i in range(2):
+    j = 2 + i
+    k = 4 + i
+    plt.plot(w[i], h[i], label="High Pass orden = %d"%(5*(i + 1)))
+    plt.plot(w[j], h[j], label="Band Pass orden = %d"%(5*(i + 1)))
+    plt.plot(w[k], h[k], label="Low Pass orden = %d"%(5*(i + 1)))
 plt.grid(True)
 plt.legend()
 plt.xlabel("Frequencia [Hz]")
@@ -74,10 +85,24 @@ plt.ylabel("Amplitud")
 plt.title("Señales filtradas")
 
 # Grafico del espectrograma
-for i in range(6):
-    plt.figure(4 + i)
-    plt.pcolormesh(ts[i],fs[i],spectrums[i])
-    plt.title("Espectrograma %d"%i)
+for i in range(2):
+    j = 2 + i
+    k = 4 + i
+    plt.figure(4 + i * 2)
+    plt.pcolormesh(ts[i],fs[i],np.log10(spectrums[i]))
+    plt.title("Espectrograma High Pass Orden = %d"%(5*(i + 1)))
+    plt.xlabel("Tiempo [s]")
+    plt.ylabel("Frecuencia [Hz]")
+    plt.colorbar().set_label("Amplitud [Db]")
+    plt.figure(5 + i * 2)
+    plt.pcolormesh(ts[k],fs[k],np.log10(spectrums[k]))
+    plt.title("Espectrograma Low Pass Orden = %d"%(5*(i + 1)))
+    plt.xlabel("Tiempo [s]")
+    plt.ylabel("Frecuencia [Hz]")
+    plt.colorbar().set_label("Amplitud [Db]")
+    plt.figure(6 + i * 2)
+    plt.pcolormesh(ts[j],fs[j],np.log10(spectrums[j]))
+    plt.title("Espectrograma Band Pass Orden = %d"%(5*(i + 1)))
     plt.xlabel("Tiempo [s]")
     plt.ylabel("Frecuencia [Hz]")
     plt.colorbar().set_label("Amplitud [Db]")
