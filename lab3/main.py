f83bf1c3c800b561136e6da6f03f9863a5b311ba
Maximiliano Arévalo <maximiliano.arevalo@usach.cl>
2019-05-16 11:09:23 -0400

    Done

--- lab3/main.py
+++ lab3/main.py
@@ -2,9 +2,11 @@
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
-
 import numpy as np
 
+#Funcion que extiende la matriz original, replicando los pixeles adyacentes y utilizandolos como borde
+#Entrada: Imagen representada como un array
+#Salida: Imagen extendida con 2 bordes como un array
 def extends(matrix):
     n, m = matrix.shape
     newMatrix = np.zeros([n + 4, m + 4], dtype=float)
@@ -27,9 +29,11 @@ def extends(matrix):
             newMatrix[n+1+i,m+1+j] = matrix.item((n-1,m-1))
             newMatrix[i,m+1+j] = matrix.item((0,m-1))
             newMatrix[n+1+i,j] = matrix.item((n-1,0))
-    #print(newMatrix)
     return newMatrix
 
+#Funcion que realiza la convolucion entre dos matrices, retorna el valor correspondiente
+#Entrada: Kernel y matriz como array y posicion a calcular la aplicacion del kernel sobre la matriz
+#Salida: Valor que posee la convolucion en el punto x,y dado
 def convolve(kernel, matrix, x, y):
     temp = 0
     n, m = kernel.shape
@@ -38,41 +42,59 @@ def convolve(kernel, matrix, x, y):
             temp += kernel.item((i, j)) * matrix.item((x+i,y+j))
     return temp
 
+#Funcion que obtiene y muestra la transformada de fourier de una imagen segun su tipo
+#Entrada: Imagen a la que se le aplicara la transformada como array y el tipo (si es original, gaussiana o con bordes)
+#Salida: Graficos de las transformadas de fourier
+def fourier2D(image,type):
+    f = np.fft.fft2(image)
+    fshift = np.fft.fftshift(f)
+    magnitude_spectrum = 20 * np.log(np.abs(fshift))
+    plt.figure(type), plt.imshow(magnitude_spectrum, cmap='gray')
+    if type == 1:
+        plt.title('Transformada de Fourier Imagen Original')
+    elif type == 2:
+        plt.title('Transformada de Fourier Imagen Gauss')
+    else:
+        plt.title('Transformada de Fourier Imagen Bordes')
+    plt.colorbar()
+    plt.show()
+
+#Apertura de la imagen y almacenamiento como array
 image = Image.open("leena512.bmp")
 array_image = np.asarray(image,dtype=float)
-#print(array_image)
+#Normalizacion de la imagen
 array_image = array_image / 255
+#Obtencion de dimensiones
 n,m = array_image.shape
+#Definicion de kernel gaussiano y de bordes
 kernelG = [[1,4,6,4,1],[4,16,24,16,4],[6,24,36,24,6],[4,16,24,16,4],[1,4,6,4,1]]
 kernelB = [[1,2,0,-2,-1],[1,2,0,-2,-1],[1,2,0,-2,-1],[1,2,0,-2,-1],[1,2,0,-2,-1]]
+#Almacenamiento como array
 kernelGauss = np.array(kernelG)
 kernelGauss = np.multiply(kernelGauss,1/256)
 kernelBordes = np.array(kernelB)
-#Test
-matrix = [[1, 2, 3, 4, 5], [1, 2, 3, 4, 5], [1, 2, 3, 4, 5], [1, 2, 3, 4, 5], [1, 2, 3, 4, 5]]
-matrixM = np.array(matrix) #esta wea convierte la wea de arriba en array de numpy
-# esta wea muestra que agranda la imagen qla
+#Se extienden los bordes de la imagen original
 imagenExtendida = extends(array_image)
-#print(imagenExtendida)
-
-newMatrix = np.zeros([n, m], dtype=float)
+#Se crean los nuevos array para imagenes con filtro
+filtroGaussiano = np.zeros([n, m], dtype=float)
+filtroBordes = np.zeros([n, m], dtype=float)
+print('Procesando imagenes')
+#Se recorre la imagen y se aplican los kernels, los valores se almacenan en su array correspondiente
 for i in range(n):
     for j in range(m):
-        newMatrix[i][j] = convolve(kernelBordes,imagenExtendida,i,j) * 255
-#print(newMatrix)
-#img = Image.fromarray(newMatrix)
-image = newMatrix.astype(np.uint8)
-out1 = Image.fromarray(image)
-out1.save('test.png')
+        filtroGaussiano[i][j] = convolve(kernelGauss,imagenExtendida,i,j) * 255
+        filtroBordes[i][j] = convolve(kernelBordes,imagenExtendida,i,j) * 255
+#Transformacion de arreglo a imagen gaussiana
+imgGauss = filtroGaussiano.astype(np.uint8)
+gauss = Image.fromarray(imgGauss)
+gauss.save('Kernel-Gauss.png')
+#Transformacion de arreglo a imagen con bordes
+imgBordes = filtroBordes.astype(np.uint8)
+bordes = Image.fromarray(imgBordes)
+bordes.save('Kernel-Bordes.png')
+print('Procesamiento finalizado')
 
-
-'''
-f = np.fft.fft2(image)
-fshift = np.fft.fftshift(f)
-magnitude_spectrum = 20*np.log(np.abs(fshift))
-plt.subplot(121),plt.imshow(image, cmap = 'gray')
-plt.title('Imagen Original'), plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(magnitude_spectrum, cmap = 'gray')
-plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
-plt.show()
-'''
+#Transformada de Fourier de la imagen original, imagen con kernel gauss e imagen con kernel de bordes
+fourier2D(array_image,1)
+fourier2D(imgGauss,2)
+fourier2D(imgBordes,3)
