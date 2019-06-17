74f06783b11dbe180960e0136bb21ca0285d3976
Alan Barahona Ruiz <alan.barahona@usach.cl>
2019-06-17 18:04:32 -0400

    Agregado funcion de ploteo y ploteo de las 6 senales

--- lab4/src/fm.py
+++ lab4/src/fm.py
@@ -26,22 +26,33 @@ def initialize_data(filename):
     return time_array, signal_array, np.array(integral_array)
 
 
+def plot(time, signal, signal_am, signal_fm, index, carrier_frequency, figure):
+    plt.figure(figure)
+    ax1 = plt.subplot(311)
+    ax1.get_xaxis().set_visible(False)
+    plt.plot(time, signal)
+    plt.title('Señal original')
+    plt.ylabel('Amplitud')
+    ax2 = plt.subplot(312,sharex = ax1)
+    ax2.get_xaxis().set_visible(False)
+    plt.plot(time, signal_am)
+    plt.title('Señal con modulación AM con índice %f y frecuencia %d'%(index, carrier_frequency))
+    plt.ylabel('Amplitud')
+    ax3 = plt.subplot(313, sharex = ax1)
+    plt.plot(time, signal_fm)
+    plt.title('Señal con modulación FM con índice %f y frecuencia %d'%(index, carrier_frequency))
+    plt.xlabel('Tiempo [x]')
+    plt.ylabel('Amplitud')
+
+
 time, signal, integral = initialize_data('../resources/handel.wav')
-signal_am = modulacion_am(signal, time, 1.0, 99100)
-signal_fm = modulacion_fm(integral, time, 1.0, 99100)
-
-error = 0.0
-for i in range(len(signal)):
-    error += np.abs(signal[i] - signal_am[i])
-error = error/len(signal)
-
-print('Error: %d'%(error))
-
-plt.figure(1)
-plt.subplot(311)
-plt.plot(time, signal)
-plt.subplot(312)
-plt.plot(time, signal_am)
-plt.subplot(313)
-plt.plot(time, signal_fm)
+signal_am_100 = modulacion_am(signal, time, 1.0, 99100)
+signal_fm_100 = modulacion_fm(integral, time, 1.0, 99100)
+signal_am_015 = modulacion_am(signal, time, 0.15, 99100)
+signal_fm_015 = modulacion_fm(integral, time, 0.15, 99100)
+signal_am_125 = modulacion_am(signal, time, 1.25, 99100)
+signal_fm_125 = modulacion_fm(integral, time, 1.25, 99100)
+plot(time, signal, signal_am_100, signal_fm_100, 1.0, 99100, 1)
+plot(time, signal, signal_am_015, signal_fm_015, 0.15, 99100, 2)
+plot(time, signal, signal_am_125, signal_fm_125, 1.25, 99100, 3)
 plt.show()
