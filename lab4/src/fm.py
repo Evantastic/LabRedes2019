54facec775043d3225f53a68e9b8bc82efb97403
Alan Barahona Ruiz <alan.barahona@usach.cl>
2019-06-18 17:51:46 -0400

    Modulacion AM funcionando

--- lab4/src/fm.py
+++ lab4/src/fm.py
@@ -6,13 +6,17 @@ from scipy.io.wavfile import read
 
 def modulacion_am(signal_array, time_array, index, carrier_frequency):
     aux = index*np.cos(2*np.pi*carrier_frequency*time_array)
-    return signal_array*aux
+    f = lambda x:x+1
+    return list(map(f,signal_array))*aux
 
 
 def modulacion_fm(integral_array, time_array, index, carrier_frequency):
     aux1 = 2*np.pi*carrier_frequency*time_array
-    aux2 = index*integral_array
-    return np.cos(aux1+aux2)
+    aux2 = 2*np.pi*index*integral_array
+    suma = []
+    for i in range(len(aux1)):
+        suma.append(aux1[i] + aux2[i])
+    return np.cos(np.array(suma))
 
 
 def initialize_data(filename):
@@ -26,11 +30,16 @@ def initialize_data(filename):
     return time_array, signal_array, np.array(integral_array)
 
 
-def plot(time, signal, signal_am, signal_fm, index, carrier_frequency, figure):
+def plot(time, signal, signal_am, signal_fm, index, carrier_frequency, figure, carrier=[]):
     plt.figure(figure)
     ax1 = plt.subplot(311)
     ax1.get_xaxis().set_visible(False)
-    plt.plot(time, signal)
+    if len(carrier) == 0:
+        plt.plot(time, signal)
+    else:
+        plt.plot(time, signal, label='Original')
+        plt.plot(time, carrier, label='Carrier')
+        plt.legend()
     plt.title('Señal original')
     plt.ylabel('Amplitud')
     ax2 = plt.subplot(312,sharex = ax1)
@@ -45,14 +54,34 @@ def plot(time, signal, signal_am, signal_fm, index, carrier_frequency, figure):
     plt.ylabel('Amplitud')
 
 
-time, signal, integral = initialize_data('../resources/handel.wav')
-signal_am_100 = modulacion_am(signal, time, 1.0, 99100)
-signal_fm_100 = modulacion_fm(integral, time, 1.0, 99100)
-signal_am_015 = modulacion_am(signal, time, 0.15, 99100)
-signal_fm_015 = modulacion_fm(integral, time, 0.15, 99100)
-signal_am_125 = modulacion_am(signal, time, 1.25, 99100)
-signal_fm_125 = modulacion_fm(integral, time, 1.25, 99100)
-plot(time, signal, signal_am_100, signal_fm_100, 1.0, 99100, 1)
-plot(time, signal, signal_am_015, signal_fm_015, 0.15, 99100, 2)
-plot(time, signal, signal_am_125, signal_fm_125, 1.25, 99100, 3)
-plt.show()
+def test(carrier_frequency):
+    f = lambda z: np.cos(z*np.pi)
+    x = np.linspace(0, 10, 40000)
+    delta = 10.0/40000.0
+    y = list(map(f,x))
+    integrales = []
+    for i in range(len(y)):
+        integrales.append(simps(y[:i+1], dx=delta))
+    integrales = np.array(integrales)
+    ya = modulacion_am(y, x, 1.0, carrier_frequency)
+    yf = modulacion_fm(integrales, x, 1.0, carrier_frequency)
+    carrier = np.cos(2*np.pi*carrier_frequency*x)
+    plot(x, y, ya, yf, 1.0, carrier_frequency, 1, carrier=carrier)
+    plt.show()
+
+    
+def main():
+    time, signal, integral = initialize_data('../resources/handel.wav')
+    signal_am_100 = modulacion_am(signal, time, 1.0, 99100)
+    signal_fm_100 = modulacion_fm(integral, time, 1.0, 99100)
+    signal_am_015 = modulacion_am(signal, time, 0.15, 99100)
+    signal_fm_015 = modulacion_fm(integral, time, 0.15, 99100)
+    signal_am_125 = modulacion_am(signal, time, 1.25, 99100)
+    signal_fm_125 = modulacion_fm(integral, time, 1.25, 99100)
+    plot(time, signal, signal_am_100, signal_fm_100, 1.0, 99100, 1)
+    plot(time, signal, signal_am_015, signal_fm_015, 0.15, 99100, 2)
+    plot(time, signal, signal_am_125, signal_fm_125, 1.25, 99100, 3)
+    plt.show()
+
+
+test(100)
